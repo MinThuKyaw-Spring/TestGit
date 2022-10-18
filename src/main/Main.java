b885ad5ab79bf13b096502971b8f7d1e19e6fc89
MinThuKyaw <minthukyaw168@gmail.com>
2022-10-18 16:44:57 +0630

    test SSH Connection

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Test SSH Connection");
 		System.out.println("Hello Git System");
 		System.out.println("Hello Git System");
 	}
